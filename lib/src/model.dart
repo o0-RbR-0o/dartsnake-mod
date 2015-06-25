8f536459a1d43076891a0cda26ac4337fb4bab0f
RbR <schwarzwurzel@sqrt.at>
2015-06-26 01:14:39 +0200

    Revert "Changed to bounding box like collision detection."
    This reverts commit 0eec7cee792b4667ab855ec5715e11c221726487.

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -14,13 +14,16 @@ class Movable_Object{
 
   }
   
-  //Detects collision with another Movable_Object with bounding boxes in size of the object. Returns true in case of collision.
+  //Detects collision with another Movable_Object. Todo: Changes positions to bounding boxes.
   bool detectCollisonWith(Movable_Object o){
-    return  ( this._position_x < o._position_x + o._sizex     &&
-              this._position_x + this._sizex > o._position_x  &&
-              this._position_y < o._position_y + o._sizey     &&
-              this._sizey + this._position_y > o._position_y
-            );
+    bool xcol=false, ycol=false;
+    if(this._position_x == o._position_x){
+      xcol=true;
+    }
+    if(this._position_y == o._position_y){
+      ycol=true;
+    }
+    return xcol&&ycol;
   }
   
   //Moves the Ffisch left 1px;
