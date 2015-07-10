e2fadd9488285fe289c99e2cb8b13e403fc876a0
RbR <schwarzwurzel@sqrt.at>
2015-07-10 20:25:51 +0200

    weapons cleaned and commented.

--- lib/src/model/game_objects/weapons.dart
+++ lib/src/model/game_objects/weapons.dart
@@ -1,11 +1,15 @@
 part of raumffisch;
 
+/*
+ * Weapons. Weapons extend the StandardWeapon class which has a shot frequency and a counter for the frequency period and implement the abstract Weapon class
+ * so we can easily swap the weapons. The shoot method creates differnt kinds of bullets and returns them in a list so we can add them to our objects list.
+ */
+
 //Abstract class to implement a weapon
 abstract class Weapon{  
   final RaumffischGame _game;
   int _sfreq_counter;
   int _shot_frequency;
-  
   Weapon(this._game);
   List<Bullet> shoot();
   void setShotFreqCount(int s);
@@ -24,7 +28,6 @@ class StandardWeapon{
    }
 }
 
-
 //Weapon shooting one small bullet right.
 class OneShot extends StandardWeapon implements Weapon{  
   OneShot(_game, _shot_frequency) : super(_game,_shot_frequency);
@@ -42,28 +45,25 @@ class OneShot extends StandardWeapon implements Weapon{
   }
 }
 
-
 //Weapon shooting a sine moving double shot.
 class SineDoubleShot extends StandardWeapon implements Weapon{  
-SineDoubleShot(_game, _shot_frequency) : super(_game,_shot_frequency);
+  SineDoubleShot(_game, _shot_frequency) : super(_game,_shot_frequency);
 
-List<Bullet> shoot(){
-  List<Bullet> bl=new List<Bullet>();
-  if(this._sfreq_counter==0){
-    Bullet p = new SineProjectile0(_game,1,0); 
-    Bullet p2 = new SineProjectile1(_game,1,0); 
-    p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2));
-    p2.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2));
-    bl.add(p);
-    bl.add(p2);
-    _game.eventSystem.shot2fired=true;
+  List<Bullet> shoot(){
+    List<Bullet> bl=new List<Bullet>();
+    if(this._sfreq_counter==0){
+      Bullet p = new SineProjectile0(_game,1,0); 
+      Bullet p2 = new SineProjectile1(_game,1,0); 
+      p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2));
+      p2.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2));
+      bl.add(p);
+      bl.add(p2);
+      _game.eventSystem.shot2fired=true;
+    }
+    this._sfreq_counter=(this._sfreq_counter+1)%this._shot_frequency;
+    return bl;
   }
-  this._sfreq_counter=(this._sfreq_counter+1)%this._shot_frequency;
-  return bl;
 }
-}
-
-
 
 //Weapon shooting 2 up/down origin toggleing small bullets right.
 class DoubleShot extends StandardWeapon implements Weapon{  
@@ -73,16 +73,15 @@ class DoubleShot extends StandardWeapon implements Weapon{
     List<Bullet> bl=new List<Bullet>();
     if(this._sfreq_counter==0){
       Bullet p = new Projectile(_game,1,0); 
-      
       p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2)+1);
       _game.eventSystem.shotfired=true;
       bl.add(p);
     }
     else if(this._sfreq_counter==(this._shot_frequency/2)){
       Bullet p = new Projectile(_game,1,0);
-            p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2)-1);
-            bl.add(p);
-            _game.eventSystem.shotfired=true;
+      p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2)-1);
+      bl.add(p);
+      _game.eventSystem.shotfired=true;
     }
     this._sfreq_counter=(this._sfreq_counter+1)%this._shot_frequency;
     return bl;
@@ -92,16 +91,13 @@ class DoubleShot extends StandardWeapon implements Weapon{
 //Weapon shooting one big bullet right while shooting 2 small bullets diagonal up+right and down+right every second shot
 class DiagonalShotWithFront extends StandardWeapon implements Weapon{  
   DiagonalShotWithFront(_game, _shot_frequency) : super(_game,_shot_frequency);
-  
   List<Bullet> shoot(){
     List<Bullet> bl=new List<Bullet>();
     if(this._sfreq_counter==0){
       Bullet p = new Projectile(_game,1,1); 
       Bullet p2 = new Projectile(_game,1,-1);
-      
       p2.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2)-1);
       p.setposition(_game._ffisch._position_x+_game._ffisch._sizex-1, _game._ffisch._position_y+(_game._ffisch._sizey ~/2)+1);
-      
       bl.add(p);
       bl.add(p2);
     }
